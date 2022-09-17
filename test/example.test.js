c21cdd8e4796401256d8db29cd230a7569b852d8
Dejan Dokic <doxa997@gmail.com>
2022-09-17 23:49:02 +0200

    Test done

--- test/example.test.js
+++ test/example.test.js
@@ -1,17 +1,27 @@
 const assert = require('assert');
 const User = require('../src/modules/user/models/user.entity');
+const sequelize = require('../src/services/db/database-setup.service');
 
 describe('Simple Math Test', () => {
   it('should return 2', () => {
     assert.equal(1 + 1, 2);
   });
+
   it('should return 9', () => {
     assert.equal(3 * 3, 9);
   });
+
   it('test user', async () => {
-    const count = await User.count();
-    await User.create({ username: 'DOXA' });
-    const count2 = await User.count();
-    assert.equal(count + 1, count2);
+    const transaction = await sequelize.transaction();
+    try {
+      const count = await User.count({ transaction });
+      await User.create({ username: 'DOXA' }, { transaction });
+      const count2 = await User.count({ transaction });
+      assert.equal(count + 1, count2);
+    } catch (err) {
+      throw err;
+    } finally {
+      await transaction.rollback();
+    }
   });
 });
