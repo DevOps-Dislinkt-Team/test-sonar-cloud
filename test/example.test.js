5dfe624ca9f2e95608a4edd2e9568dd112d841c8
Dejan Dokic <doxa997@gmail.com>
2022-09-17 19:58:15 +0200

    minor

--- test/example.test.js
+++ test/example.test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const User = require('../src/modules/user/models/user.entity');
 
 describe('Simple Math Test', () => {
   it('should return 2', () => {
@@ -6,5 +7,11 @@ describe('Simple Math Test', () => {
   });
   it('should return 9', () => {
     assert.equal(3 * 3, 9);
+  });
+  it('test user', async () => {
+    const count = await User.count();
+    await User.create({ username: 'DOXA' });
+    const count2 = await User.count();
+    assert.equal(count + 1, count2);
   });
 });
